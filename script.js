292953a0ee033b352261947b82659a83bede4144
Gajanan <115416756+gajanan0210@users.noreply.github.com>
2023-04-10 20:12:33 +0530

    some changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ content = document.querySelector(".content"),
 selectMenu = document.querySelectorAll("select"),
 setAlarmBtn = document.querySelector("button");
 let alarmTime, isAlarmSet,
-ringtone = new Audio("./files/ringtone.mp3");
+
 for (let i = 12; i > 0; i--) {
     i = i < 10 ? `0${i}` : i;
     let option = `<option value="${i}">${i}</option>`;
@@ -35,12 +35,9 @@ setInterval(() => {
     s = s < 10 ? "0" + s : s;
     currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
     if (alarmTime === `${h}:${m} ${ampm}`) {
-        ringtone.play();
-        ringtone.loop = true;
+        
         setTimeout(() => {
             alert("Time's up!");
-            ringtone.pause();
-            ringtone.currentTime = 0;
             isAlarmSet = false;
             content.classList.remove("disable");
             setAlarmBtn.innerText = "Set Alarm";
@@ -51,7 +48,7 @@ setInterval(() => {
 function setAlarm() {
     if (isAlarmSet) {
         alarmTime = "";
-        ringtone.pause();
+       
         content.classList.remove("disable");
         setAlarmBtn.innerText = "Set Alarm";
         return isAlarmSet = false;
